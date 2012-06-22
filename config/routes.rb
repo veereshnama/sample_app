c2fa051b65faedccc576c8c60cd4c8208e76dc1a
Veeresh Nama <veereshnama@me.com>
2012-06-22 04:50:47 -0400

    -Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
   
+  get "user/new"
   root to: 'static_pages#home'
+  match '/signup',  to: 'user#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contacts' 
